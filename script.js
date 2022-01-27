85b90ebe1213443229a822eda664d43aa841ec56
Nicoleta Faina <nicoletafaina.contact@gmail.com>
2022-01-27 17:12:43 +0000

    Add ng-repeat with AngularJS

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 angular.module("listApp", [])
   .controller("GuestListController", function($scope)  {
-
+    $scope.items = ["Super Guest", "Questionable Guest", "Adventurous Guest"];
+    $scope.newItem = "";
 });
