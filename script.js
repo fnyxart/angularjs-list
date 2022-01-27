b1ee1690cf9a5deb22175231dc0f8aaae15f911c
Nicoleta Faina <nicoletafaina.contact@gmail.com>
2022-01-27 17:22:42 +0000

    Implement add guest feature with AngularJS

--- script.js
+++ script.js
@@ -2,4 +2,10 @@ angular.module("listApp", [])
   .controller("GuestListController", function($scope)  {
     $scope.items = ["Super Guest", "Questionable Guest", "Adventurous Guest"];
     $scope.newItem = "";
+    $scope.pushItem = function () {
+      if ($scope.newItem != "") {
+        $scope.items.push($scope.newItem);
+        $scope.newItem = "";
+      }
+    }
 });
